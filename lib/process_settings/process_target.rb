572d7d5a586011861f6fa95b70b0398cd768f07b
Colin Kelley <colindkelley@gmail.com>
2019-09-10 22:31:56 -0700

    TECH-3107: break with_static_context into several methods

--- lib/process_settings/process_target.rb
+++ lib/process_settings/process_target.rb
@@ -31,38 +31,50 @@ module ProcessSettings
       def with_static_context(target_value, static_context_hash)
         case target_value
         when Array
-          target_value.any? do |value|
-            with_static_context(value, static_context_hash)
-          end
+          with_static_context_array(target_value, static_context_hash)
         when Hash
-          result = target_value.reduce({}) do |hash, (key, value)|
-            if static_context_hash.has_key?(key)
-              context_at_key = static_context_hash[key]
-              sub_value = with_static_context(value, context_at_key)
-              case sub_value
-              when true   # this hash entry is true, so omit it
-              when false  # this hash entry is false, so hash is false
-                return false
-              else
-                raise ArgumentError, "Got #{sub_value.inspect}???"
-              end
-            else
-              hash[key] = value
-            end
-            hash
-          end
-
-          if result.any?
-            result
-          else
-            true
-          end
+          with_static_context_hash(target_value, static_context_hash)
         when true, false
           !target_value == !static_context_hash
         else
           target_value == static_context_hash
         end
       end
+
+      private
+
+      def with_static_context_array(target_value, static_context_hash)
+        target_value.any? do |value|
+          with_static_context(value, static_context_hash)
+        end
+      end
+
+      def with_static_context_hash(target_value, static_context_hash)
+        result = target_value.reduce({}) do |hash, (key, value)|
+          if static_context_hash.has_key?(key)
+            context_at_key = static_context_hash[key]
+            sub_value = with_static_context(value, context_at_key)
+            case sub_value
+            when true   # this hash entry is true, so omit it
+            when false  # this hash entry is false, so hash is false
+              return false
+            else
+              raise ArgumentError, "Got #{sub_value.inspect}???"
+            end
+          else
+            hash[key] = value
+          end
+          hash
+        end
+
+        if result.any?
+          result
+        else
+          true
+        end
+      end
+
+      public
 
       def target_key_matches?(target_value, context_hash)
         case target_value
