09bdcd6df7b455180b110a54ef00b9e374cc707d
Colin Kelley <colindkelley@gmail.com>
2019-09-15 17:56:47 -0700

    TECH-3698: process_settings_for_services.rb WIP

--- bin/process_settings_for_services.rb
+++ bin/process_settings_for_services.rb
@@ -2,53 +2,108 @@
 # frozen_string_literal: true
 
 require_relative '../lib/process_settings/targeted_process_settings'
+require 'optparse'
+require 'ostruct'
 require 'yaml'
+require 'set'
 
 require 'pry'
 
+CORRELATION_KEYS = [
+  ['node'],
+  ['app'],
+  ['region'],
+  ['app', 'node'],
+  ['app', 'region'],
+  ['deployGroup'],
+  ['app', 'deployGroup']
+].freeze
+
 PROGRAM_NAME = File.basename($PROGRAM_NAME)
 
-def check_options(argv)
-  unless argv.size == 2
-    warn "usage: #{PROGRAM_NAME} <path-to-flat_services.yml> <path-to-combined_process_settings.yml"
-    exit(1)
+def parse_options(argv)
+  options = OpenStruct.new
+  options.verbose = false
+  option_parser = OptionParser.new(argv) do |opt|
+    opt.op('-o', '--output', 'Output file.') { |o| options.output_filename = o }
   end
 
-  argv
+  if option_parser.parse! && argv.size == 3 && options.output_filename
+    argv
+  else
+    warn "usage: #{PROGRAM_NAME} <path-to-flat_services.yml> <path-to-combined_process_settings-OLD.yml <path-to-combined_process_settings-NEW.yml -o output_filename"
+    option_parser.summarize(STDERR)
+    exit(1)
+  end
 end
 
 
 #
 # main
 #
-path_to_flat_services_yml, path_to_combined_process_settings_yml = check_options(ARGV.freeze)
+path_to_flat_services_yml, path_to_combined_process_settings_yml_old, path_to_combined_process_settings_yml_new = parse_options(ARGV.dup)
 
 flat_services_yml_json_doc     = YAML.load_file(path_to_flat_services_yml)
-combined_process_settings_yml_json_doc = YAML.load_file(path_to_combined_process_settings_yml)
+combined_process_settings_yml_json_doc_old = YAML.load_file(path_to_combined_process_settings_yml_old)
+combined_process_settings_yml_json_doc_new = YAML.load_file(path_to_combined_process_settings_yml_new)
 
-targeted_process_settings = ProcessSettings::TargetedProcessSettings.from_array(combined_process_settings_yml_json_doc)
+targeted_process_settings_old = ProcessSettings::TargetedProcessSettings.from_array(combined_process_settings_yml_json_doc_old)
+targeted_process_settings_new = ProcessSettings::TargetedProcessSettings.from_array(combined_process_settings_yml_json_doc_new)
 
-flat_services_with_targeted_settings = {}
+flat_services_with_targeted_settings = Hash.new { |h, service| h[service] = Hash.new { |h2, node| h2[node] = [] } }
+
+correlation_scorecard = Hash.new { |h, k| h[k] = { false => Set.new, true => Set.new } }
 
 flat_services_yml_json_doc.each do |service, nodes|
-  flat_services_with_targeted_settings[service] = {}
   nodes.each do |node, pods|
-    flat_services_with_targeted_settings[service][node] ||= []
     pods.each_with_index do |pod_context, pod_index|
       node_attrs = pod_context.dup
+      node_attrs['service']   = service
+      node_attrs['node']      = node
+      node_attrs['pod_index'] = pod_index if pods.size > 1
+      node_targeted_process_settings_old = targeted_process_settings_old.with_static_context(pod_context)
+      node_targeted_process_settings_new = targeted_process_settings_new.with_static_context(pod_context)
+
       flat_services_with_targeted_settings[service][node] << node_attrs
-      node_targeted_process_settings = targeted_process_settings.with_static_context(pod_context)
 
-      node_attrs['__targeted_process_settings'] = []
+      if (changed = node_targeted_process_settings_old != node_targeted_process_settings_new)
+        node_old =  node_targeted_process_settings_old.map do |node_targeted_process_setting|
+                      {
+                        'target'            => node_targeted_process_setting.target,
+                        "process_settings"  => node_targeted_process_setting.process_settings
+                      }
+                    end
+        node_new =  node_targeted_process_settings_new.map do |node_targeted_process_setting|
+                      {
+                        'target'            => node_targeted_process_setting.target,
+                        "process_settings"  => node_targeted_process_setting.process_settings
+                      }
+                    end
 
-      node_targeted_process_settings.each do |node_targeted_process_setting|
-        node_attrs['__targeted_process_settings'] << {
-          'target' => node_targeted_process_setting.target,
-           "process_settings" => node_targeted_process_setting.process_settings
+        node_attrs['__changes__'] = {
+          'old' => node_old,
+          'new' => node_new
         }
+        node_attrs['__changes__']['pod_index'] = pod_index if pods.size > 1
+      end
+
+      CORRELATION_KEYS.each do |correlation_key|
+        scorecard = correlation_scorecard[correlation_key]
+
+        correlation_key_values = correlation_key.map { |key| { key => node_attrs[key] } }
+        scorecard[changed] << correlation_key_values
       end
     end
   end
 end
+
+
+perfect_correlations = correlation_scorecard.select do |_correllation_key, scorecard|
+  (scorecard[false] & scorecard[true]).empty?
+end
+
+best_correlation = perfect_correlations.min_by { |_correllation_key, scorecard| scorecard[true].size }
+
+puts best_correlation.first.inspect, best_correlation.last[true].to_a.inspect
 
 puts flat_services_with_targeted_settings.to_yaml
