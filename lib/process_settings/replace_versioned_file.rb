171b04ee0a9b881bc2eae7f6929238944ab796e0
Juan Leal <jleal@invoca.com>
2019-10-15 10:09:47 -0500

    Changes method and class names to be more descriptive

--- lib/process_settings/replace_versioned_file.rb
+++ lib/process_settings/replace_versioned_file.rb
@@ -4,21 +4,17 @@ require_relative 'targeted_settings'
 
 module ProcessSettings
   # This class will override a file with a higher version file; it accounts for minor version number use
-  class CopyVersionedFile
+  class ReplaceVersionedFile
 
-    def copy_file(source_file_name, destination_file_name)
-      if copy_file?(source_file_name, destination_file_name)
-        FileUtils.cp(source_file_name, destination_file_name)
-      end
-    ensure
-      if source_file_name != destination_file_name # avoid removing source file if destination file is the same
-        FileUtils.remove_file(destination_file_name)
+    def replace_file_on_newer_file_version(source_file_name, destination_file_name)
+      if source_version_is_newer?(source_file_name, destination_file_name)
+        FileUtils.mv(source_file_name, destination_file_name)
       end
     end
 
     private
 
-    def copy_file?(source_file_name, destination_file_name)
+    def source_version_is_newer?(source_file_name, destination_file_name)
       source_version      = ProcessSettings::TargetedSettings.from_file(source_file_name, only_meta: true).version
       destination_version = ProcessSettings::TargetedSettings.from_file(destination_file_name, only_meta: true).version
 
