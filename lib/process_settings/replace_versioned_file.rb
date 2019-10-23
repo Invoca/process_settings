8ed7cea0fb5818decb68dbe96bc6ee83a9da6c32
Juan Leal <jleal@invoca.com>
2019-10-23 16:19:54 -0500

    Adds contracts to ProcessSettings::ReplaceVersionedFile

--- lib/process_settings/replace_versioned_file.rb
+++ lib/process_settings/replace_versioned_file.rb
@@ -5,7 +5,15 @@ require_relative 'targeted_settings'
 module ProcessSettings
   # This class will override a file with a higher version file; it accounts for minor version number use
   module ReplaceVersionedFile
+    class SourceVersionOlderError < StandardError; end
+    class FileDoesNotExistError < StandardError; end
+
     class << self
+      # Contracts
+      #   source_file_path must be present
+      #   destination_file_path must be present
+      #   source_file_path must exist on filesystem
+      #   source file version has to greater than or equal to destination version
       def replace_file_on_newer_file_version(source_file_path, destination_file_path)
         source_file_path.to_s      == '' and raise ArgumentError, "source_file_path not present"
         destination_file_path.to_s == '' and raise ArgumentError, "destination_file_path not present"
@@ -13,24 +21,33 @@ module ProcessSettings
         if source_version_is_newer?(source_file_path, destination_file_path)
           FileUtils.mv(source_file_path, destination_file_path)
         elsif source_file_path != destination_file_path # make sure we're not deleting destination file
-          if File.exist?(source_file_path)
-            FileUtils.remove_file(source_file_path) # clean up, remove left over file
-          end
+          remove_source_file(source_file_path) # clean up, remove left over file
         end
       end
 
       private
 
       def source_version_is_newer?(source_file_path, destination_file_path)
-        if File.exist?(source_file_path)
-          if File.exist?(destination_file_path)
-            source_version      = ProcessSettings::TargetedSettings.from_file(source_file_path, only_meta: true).version
-            destination_version = ProcessSettings::TargetedSettings.from_file(destination_file_path, only_meta: true).version
+        !File.exist?(source_file_path) and raise FileDoesNotExistError, "source file '#{source_file_path}' does not exist"
+        !File.exist?(destination_file_path) and return true
 
-            Gem::Version.new(source_version) > Gem::Version.new(destination_version)
-          else
-            true
-          end
+        source_version      = ProcessSettings::TargetedSettings.from_file(source_file_path, only_meta: true).version
+        destination_version = ProcessSettings::TargetedSettings.from_file(destination_file_path, only_meta: true).version
+
+        if Gem::Version.new(source_version) > Gem::Version.new(destination_version)
+          true
+        elsif Gem::Version.new(source_version) < Gem::Version.new(destination_version)
+          remove_source_file(source_file_path) # clean up, remove left over file
+
+          raise SourceVersionOlderError,
+                "source file '#{source_file_path}' is version #{source_version}"\
+                " and destination file '#{destination_file_path}' is version #{destination_version}"
+        end
+      end
+
+      def remove_source_file(source_file_path)
+        if File.exist?(source_file_path)
+          FileUtils.remove_file(source_file_path) # clean up, remove left over file
         end
       end
     end
--- spec/lib/process_settings/replace_versioned_file_spec.rb
+++ spec/lib/process_settings/replace_versioned_file_spec.rb
@@ -13,7 +13,7 @@ describe ProcessSettings::ReplaceVersionedFile do
 
   describe "replace_file_on_newer_file_version" do
     context "when source file path is not provided" do
-      it "raises an error" do
+      it "raises an ArgumentError" do
         params = ["", combined_settings_v16]
 
         expect { described_class.replace_file_on_newer_file_version(*params) }.to raise_error(ArgumentError, "source_file_path not present")
@@ -21,7 +21,7 @@ describe ProcessSettings::ReplaceVersionedFile do
     end
 
     context "when destination path is not provided" do
-      it "raises an error" do
+      it "raises an ArgumentError" do
         params = [combined_settings_v18, ""]
 
         expect { described_class.replace_file_on_newer_file_version(*params) }.to raise_error(ArgumentError, "destination_file_path not present")
@@ -38,13 +38,16 @@ describe ProcessSettings::ReplaceVersionedFile do
       end
     end
 
-    context "when source file does not exists" do
-      it "should replace the file" do
+    context "when source file does not exist" do
+      it "raises FileDoesNotExistError" do
         params = ["spec/fixtures/production/combined_process_settings-xxx.yml", combined_settings_v18]
 
         expect(FileUtils).to_not receive(:mv).with(*params)
         expect(FileUtils).to_not receive(:remove_file)
-        described_class.replace_file_on_newer_file_version(*params)
+
+        expect { described_class.replace_file_on_newer_file_version(*params) }
+          .to raise_error(ProcessSettings::ReplaceVersionedFile::FileDoesNotExistError,
+                          "source file 'spec/fixtures/production/combined_process_settings-xxx.yml' does not exist")
       end
     end
 
@@ -98,15 +101,7 @@ describe ProcessSettings::ReplaceVersionedFile do
       end
     end
 
-    context "when source major verion is equal to destination major version and no minor versions" do
-      it "should not replace the file" do
-        params = [combined_settings_v16, combined_settings_v16]
-
-        expect(FileUtils).to_not receive(:mv).with(*params)
-        expect(FileUtils).to_not receive(:remove_file)
-        described_class.replace_file_on_newer_file_version(*params)
-      end
-
+    context "when source major verion is equal to destination major version" do
       context "when source minor version is not specified and destination minor version is zero" do
         it "should not replace the file" do
           params = [combined_settings_v16, combined_settings_v16_0]
@@ -128,12 +123,16 @@ describe ProcessSettings::ReplaceVersionedFile do
       end
 
       context "when source minor version is not specified and destination minor version is greater than zero" do
-        it "should not replace the file" do
+        it "should not replace the file and raises SourceVersionOlderError" do
           params = [combined_settings_v16, combined_settings_v16_50]
 
           expect(FileUtils).to_not receive(:mv).with(*params)
           expect(FileUtils).to receive(:remove_file)
-          described_class.replace_file_on_newer_file_version(*params)
+
+          expect { described_class.replace_file_on_newer_file_version(*params) }
+            .to raise_error(ProcessSettings::ReplaceVersionedFile::SourceVersionOlderError,
+                            "source file 'spec/fixtures/production/combined_process_settings-16.yml' is version 16"\
+                            " and destination file 'spec/fixtures/production/combined_process_settings-16-50.yml' is version 16.5")
         end
       end
 
@@ -148,12 +147,16 @@ describe ProcessSettings::ReplaceVersionedFile do
       end
 
       context "when source minor version is less than destination minor version" do
-        it "should not replace the file" do
+        it "should not replace the file and raises SourceVersionOlderError" do
           params = combined_settings_v16_0, combined_settings_v16_50
 
           expect(FileUtils).to_not receive(:mv).with(*params)
           expect(FileUtils).to receive(:remove_file)
-          described_class.replace_file_on_newer_file_version(*params)
+
+          expect { described_class.replace_file_on_newer_file_version(*params) }
+            .to raise_error(ProcessSettings::ReplaceVersionedFile::SourceVersionOlderError,
+                            "source file 'spec/fixtures/production/combined_process_settings-16-0.yml' is version 16.0"\
+                            " and destination file 'spec/fixtures/production/combined_process_settings-16-50.yml' is version 16.5")
         end
       end
 
@@ -169,51 +172,71 @@ describe ProcessSettings::ReplaceVersionedFile do
     end
 
     context "when source major verion is less than destination major version" do
-      it "should not replace the file" do
+      it "should not replace the file and raises SourceVersionOlderError" do
         params = [combined_settings_v16, combined_settings_v18]
 
         expect(FileUtils).to_not receive(:mv).with(*params)
         expect(FileUtils).to receive(:remove_file)
-        described_class.replace_file_on_newer_file_version(*params)
+
+        expect { described_class.replace_file_on_newer_file_version(*params) }
+          .to raise_error(ProcessSettings::ReplaceVersionedFile::SourceVersionOlderError,
+                          "source file 'spec/fixtures/production/combined_process_settings-16.yml' is version 16"\
+                          " and destination file 'spec/fixtures/production/combined_process_settings-18.yml' is version 18")
       end
 
       context "when source minor version is not specified and destination minor is not zero" do
-        it "should not replace the file" do
+        it "should not replace the file and raises SourceVersionOlderError" do
           params = [combined_settings_v16, combined_settings_v18_1]
 
           expect(FileUtils).to_not receive(:mv).with(*params)
           expect(FileUtils).to receive(:remove_file)
-          described_class.replace_file_on_newer_file_version(*params)
+
+          expect { described_class.replace_file_on_newer_file_version(*params) }
+            .to raise_error(ProcessSettings::ReplaceVersionedFile::SourceVersionOlderError,
+                            "source file 'spec/fixtures/production/combined_process_settings-16.yml' is version 16"\
+                            " and destination file 'spec/fixtures/production/combined_process_settings-18-1.yml' is version 18.1")
         end
       end
 
       context "when source minor version is not zero and destination minor version is not specified" do
-        it "should not replace the file" do
+        it "should not replace the file and raises SourceVersionOlderError" do
           params = combined_settings_v16_50, combined_settings_v18
 
           expect(FileUtils).to_not receive(:mv).with(*params)
           expect(FileUtils).to receive(:remove_file)
-          described_class.replace_file_on_newer_file_version(*params)
+
+          expect { described_class.replace_file_on_newer_file_version(*params) }
+            .to raise_error(ProcessSettings::ReplaceVersionedFile::SourceVersionOlderError,
+                            "source file 'spec/fixtures/production/combined_process_settings-16-50.yml' is version 16.5"\
+                            " and destination file 'spec/fixtures/production/combined_process_settings-18.yml' is version 18")
         end
       end
 
       context "when source minor version is less than destination minor version" do
-        it "should not replace the file" do
+        it "should not replace the file and raises SourceVersionOlderError" do
           params = combined_settings_v16_0, combined_settings_v18_1
 
           expect(FileUtils).to_not receive(:mv).with(*params)
           expect(FileUtils).to receive(:remove_file)
-          described_class.replace_file_on_newer_file_version(*params)
+
+          expect { described_class.replace_file_on_newer_file_version(*params) }
+            .to raise_error(ProcessSettings::ReplaceVersionedFile::SourceVersionOlderError,
+                            "source file 'spec/fixtures/production/combined_process_settings-16-0.yml' is version 16.0"\
+                            " and destination file 'spec/fixtures/production/combined_process_settings-18-1.yml' is version 18.1")
         end
       end
 
       context "when source minor version is greater than destination minor version" do
-        it "should not replace the file" do
+        it "should not replace the file and raises SourceVersionOlderError" do
           params = combined_settings_v16_50, combined_settings_v18_1
 
           expect(FileUtils).to_not receive(:mv).with(*params)
           expect(FileUtils).to receive(:remove_file)
-          described_class.replace_file_on_newer_file_version(*params)
+
+          expect { described_class.replace_file_on_newer_file_version(*params) }
+            .to raise_error(ProcessSettings::ReplaceVersionedFile::SourceVersionOlderError,
+                            "source file 'spec/fixtures/production/combined_process_settings-16-50.yml' is version 16.5"\
+                            " and destination file 'spec/fixtures/production/combined_process_settings-18-1.yml' is version 18.1")
         end
       end
     end
