f2917ff4a81e4793cf0b681673cde89d08fc2f4b
James Ebentier <jebentier@invoca.com>
2020-04-20 21:57:18 +0200

    TECH-3417: add [] method to monitor stub

--- spec/lib/process_settings/testing/monitor_stub_spec.rb
+++ spec/lib/process_settings/testing/monitor_stub_spec.rb
@@ -34,4 +34,24 @@ describe ProcessSettings::Testing::MonitorStub do
       expect { subject.targeted_value('honeypot', 'unknown', dynamic_context: {}) }.to raise_exception(ProcessSettings::SettingsPathNotFound)
     end
   end
+
+  describe "#[]" do
+    it "returns values when found" do
+      result = subject['honeypot', 'answer_odds', dynamic_context: {}]
+      expect(result).to eq(100)
+    end
+
+    it "returns nil when not found and required: false" do
+      result = subject['honeypot', 'unknown', dynamic_context: {}, required: false]
+      expect(result).to eq(nil)
+    end
+
+    it "raises exception when not found and explicit required: true" do
+      expect { subject['honeypot', 'unknown', dynamic_context: {}, required: true] }.to raise_exception(ProcessSettings::SettingsPathNotFound)
+    end
+
+    it "raises exception when not found and default required: true" do
+      expect { subject['honeypot', 'unknown', dynamic_context: {}] }.to raise_exception(ProcessSettings::SettingsPathNotFound)
+    end
+  end
 end
