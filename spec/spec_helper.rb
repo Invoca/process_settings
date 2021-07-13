57bb80c355727de99d23f08503da422ee9b12d23
Yinon Rousso <yrousso@invoca.com>
2021-07-13 14:32:47 -0700

    TECH-5976 fix simplecov config in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,21 +4,21 @@ require 'rspec_junit_formatter'
 require 'process_settings'
 
 require 'pry'
-require 'simplecov'
 
-SimpleCov.start 'rails' do
-  if ENV['CI']
-    require 'simplecov-lcov'
+if ENV['GITHUB_ACTIONS'].presence
+  require 'simplecov'
+  require 'simplecov-lcov'
 
+  SimpleCov.start do
     SimpleCov::Formatter::LcovFormatter.config do |c|
       c.report_with_single_file = true
       c.single_report_path = 'coverage/lcov.info'
     end
 
     formatter SimpleCov::Formatter::LcovFormatter
-  end
 
-  add_filter %w[version.rb initializer.rb]
+    add_filter %w[version.rb initializer.rb]
+  end
 end
 
 RSpec.configure do |config|
