a1e8610c96d1b09b4c8fd4f7654a9c670558034f
Yinon Rousso <yrousso@invoca.com>
2021-07-13 13:05:40 -0700

    TECH-5976 use simplecov for gh actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,22 @@ require 'rspec_junit_formatter'
 require 'process_settings'
 
 require 'pry'
-require 'coveralls'
-Coveralls.wear!
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  if ENV['CI']
+    require 'simplecov-lcov'
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+
+  add_filter %w[version.rb initializer.rb]
+end
 
 RSpec.configure do |config|
   config.add_formatter  :progress
