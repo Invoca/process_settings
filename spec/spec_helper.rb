895f3d6cf140be02bc8f1c4178c4c1a7b9a66646
Omeed Rabani <omeedrabani@gmail.com>
2023-11-28 12:09:46 -0800

    non-production update readme with targeting best practices: Require activesupport/core_ext/object/blank in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,11 @@
 
 require 'rspec_junit_formatter'
 require 'process_settings'
+require 'active_support/core_ext/object/blank'
 
 require 'pry'
 
-if ENV['GITHUB_ACTIONS'] && !ENV['GITHUB_ACTIONS'].strip.empty?
+if ENV['GITHUB_ACTIONS'].presence
   require 'simplecov'
   require 'simplecov-lcov'
 
@@ -22,15 +23,8 @@ if ENV['GITHUB_ACTIONS'] && !ENV['GITHUB_ACTIONS'].strip.empty?
 end
 
 RSpec.configure do |config|
-  formatter_outpath_path =
-    if ENV['JUNIT_OUTPUT'] && !ENV['JUNIT_OUTPUT'].strip.empty?
-      ENV['JUNIT_OUTPUT']
-    else
-      'spec/reports/rspec.xml'
-    end
-
   config.add_formatter  :progress
-  config.add_formatter  RspecJunitFormatter, formatter_outpath_path
+  config.add_formatter  RspecJunitFormatter, ENV['JUNIT_OUTPUT'].presence || 'spec/reports/rspec.xml'
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
